0ea11768d1026fa8c97dc6c5912514556ce3af7e
David Lewitan <dovidlwtn@gmail.com>
2016-10-11 14:31:58 -0400

    add verticals and groups

--- scripts/dataSlicing.js
+++ scripts/dataSlicing.js
@@ -23,7 +23,49 @@ function sudokuStructure(dom) {
 	}
 
 	function getVerticals(arr) {
+		var width = dom.box_count * dom.box_width,
+			verticals = pushToArr([], width, function(){return [] }),
+			counter = 0;
 
+		for (var i = 0; i < width; i++) {
+			for (var j = 0; j < width; j++) {
+				verticals[j].push(arr[counter]);
+				counter++;
+			};
+		};
+
+		return verticals
+	}
+
+	function getGroups(arr) {
+		var gWidth = dom.box_width, //makes the var name shorter
+			gCount = dom.box_count, // ^^^
+			gLength = gWidth * gWidth, // how many boxes in each group
+			width = gCount * gWidth, // how wide is the grid
+			groups = pushToArr([], gCount * gCount, function(){return [] }), //create empty array of arrays
+			counter = 0, // help keep track
+			groupIndex = 0, hr=[];
+
+		hr = arr.map(function(unit, i) {
+			var place = gWidth,
+			    iters = 0;
+			while (counter >= place) {
+				place += gWidth;
+				iters++;
+			}
+			counter++
+			if (counter==width) {counter = 0};
+			return iters;
+		});
+
+		for (var i = 0; i < arr.length; i++) {
+			groupIndex = hr[i];
+			while (groups[groupIndex].length == gLength) {groupIndex += gCount }
+			arr[i].group = groupIndex;
+			groups[groupIndex].push(arr[i]);
+		};
+
+		return groups
 	}
 
 	var board_logic = function() {
@@ -43,8 +85,8 @@ function sudokuStructure(dom) {
 		});
 
 		horizontals = getHorizontals(mainArr);
-		// verticals = getVerticals()
-		// groups = getGroups()
+		verticals = getVerticals(mainArr);
+		groups = getGroups(mainArr)
 
 		return {
 			mainArr: mainArr,
--- scripts/helperFuncs.js
+++ scripts/helperFuncs.js
@@ -4,3 +4,11 @@ function getInt(str) {
 
 	return num ? Number(num) : 3;
 }
+
+function pushToArr(arr, num, rtVal) {
+	for (var i = 0; i < num; i++) {
+		arr.push(rtVal());
+	};
+
+	return arr
+}
